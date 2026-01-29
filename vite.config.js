c09f3137825ccff8ba6d9f3559deeaa1391f7858
irsakanwal552 <irsakanwal552@gmail.com>
2026-01-30 01:16:11 +0500

    third commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 
 // https://vite.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),
-],
-base:'/watch-website/'
+  plugins: [react(), tailwindcss()],
+  base: '/',
 })
